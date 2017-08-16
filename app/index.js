2dd4ee9177f3ceea1ebbd8b5100194044c0fb18f
Satu S <satu.salekari@gmail.com>
2017-08-16 13:45:19 +0300

    Add single app root for all platforms

---
+++ app/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>
+      Welcome to React Native with new App page!
+    </Text>
+    <Text style={styles.instructions}>
+      Hello hello hello
+    </Text>
+    <Text style={styles.instructions}>
+      ksfnsklfnsdlkfnsdkl
+    </Text>
+  </View>
+);
