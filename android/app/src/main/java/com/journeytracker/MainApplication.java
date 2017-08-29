92014d5a89cbd66a990fd2f5879439e6fcc22cde
Satu Salekari <satu@Satus-MBP.localdomain>
2017-08-29 12:12:15 +0300

    Upgrade react and re-eject /ios and /android
    To fix this issue:
    https://github.com/facebook/react-native/issues/14423

--- android/app/src/main/java/com/journeytracker/MainApplication.java
+++ android/app/src/main/java/com/journeytracker/MainApplication.java
@@ -3,10 +3,6 @@ package com.journeytracker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnative.photoview.PhotoViewPackage;
-import cl.json.RNSharePackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,11 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new PhotoViewPackage(),
-            new RNSharePackage(),
-            new LinearGradientPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
   };
